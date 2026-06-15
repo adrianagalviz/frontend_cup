47e61cccb9c7e11b262e3979cd86e59fc24e0c94
adriana <adrianagalviz13@gmail.com>
2026-06-15 01:23:41 -0400

    nube

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://frontend-cup.vercel.app/'
 export const API_VERSION_PREFIX = '/v1'
 
